3b0b7249e024777618d7514ab256ed13c74c73b8
Jeremie Roy <jeremie.roy@gmail.com>
2013-02-13 20:12:53 +0100

    add stub shaders

---
+++ shaders/common.sh
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2011-2013 Branimir Karadzic. All rights reserved.
+ * License: http://www.opensource.org/licenses/BSD-2-Clause
+ */
+
+#ifndef __SHADER_COMMON_H__
+#define __SHADER_COMMON_H__
+
+#include "bgfx_shader.sh"
+
+uniform mat4 u_view;
+uniform mat4 u_viewProj;
+uniform mat4 u_model;
+uniform mat4 u_modelView;
+uniform mat4 u_modelViewProj;
+uniform mat4 u_modelViewProjX;
+uniform mat4 u_viewProjX;
+
+#endif // __SHADER_COMMON_H__
