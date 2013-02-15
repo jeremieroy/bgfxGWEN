9adcf72f5027fe30d981ddbf293f0b7b6c515380
Jeremie Roy <jeremie.roy@gmail.com>
2013-02-15 12:39:55 +0100

    Add initial GlyphStash implementation

---
+++ Renderer/GlyphStash.cpp
@@ -0,0 +1,244 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+#include <assert.h>
+//#include <limits.h>
+#include "GlyphStash.h"
+
+#define BGFX_FONT_ASSERT(cond, message) assert((cond) && (message));
+
+namespace bgfx_font
+{
+     
+GlyphStash::GlyphStash(ITextureProvider* texture)
+{
+    BGFX_FONT_ASSERT(texture != NULL, "A texture provider cannot be NULL");
+
+    texture->init();
+
+    uint32_t depth = texture->getDepth();
+    uint32_t width = texture->getDepth();
+    uint32_t height = texture->getDepth();
+    
+    BGFX_FONT_ASSERT( (depth == 1) || (depth == 4) , "Incompatible texture depth, must be 1 or 4");
+    
+    m_width = width;
+    m_height = height;
+    m_depth = depth;
+    m_texture = texture;
+    m_usedSpace = 0;
+    // We want a one pixel border around the whole atlas to avoid any artefact when
+    // sampling texture
+    Node node(1,1, width-2);
+    m_skyline.push_back(node);
+}
+
+GlyphStash::~GlyphStash()
+{
+    m_texture->shutdown();
+}
+
+void GlyphStash::updateRegion(Rect16 rect, const char* data, uint32_t stride)
+{   
+    assert( rect.x < (m_width-1));
+    assert( (uint32_t)(rect.x + rect.w) <= (uint32_t)(m_width-1));
+    assert( rect.y < (m_height-1));
+    assert( (uint32_t)(rect.y + rect.h) <= (uint32_t)(m_height-1));
+
+
+    m_texture->update(rect, data, stride);
+    /*
+    size_t i;
+    size_t depth = self->depth;
+    size_t charsize = sizeof(char);
+
+    size_t height = rect.h;
+    for( size_t i = 0; i < height; ++i )
+    {
+        memcpy( self->data+((y+i)*self->width + x ) * charsize * depth, 
+                data + (i*stride) * charsize, width * charsize * depth  );
+    }*/
+}
+
+float GlyphStash::getUsageRatio()
+{
+    return 0;
+}
+
+void GlyphStash::clear()
+{  
+    m_skyline.clear();
+    m_usedSpace = 0;
+    
+    // We want a one pixel border around the whole atlas to avoid any artefact when
+    // sampling texture
+    Node node(1,1, m_width-2);
+    m_skyline.push_back(node);
+    m_texture->clear();
+}
+
+int32_t GlyphStash::fit(uint32_t skylineNodeIndex, uint16_t _width, uint16_t _height)
+{
+    uint32_t width = _width;
+    uint32_t height = _height;
+    
+    Node& baseNode = m_skyline[skylineNodeIndex];
+    
+    uint32_t x = baseNode.x, y, width_left = width;
+	uint32_t i = skylineNodeIndex;
+
+    if ( (x + width) > (m_width-1) )
+    {
+		return -1;
+    }
+    y = baseNode.y;
+	while( width_left > 0 )
+	{
+        Node& node = m_skyline[i];
+        if( node.y > y )
+        {
+            y = node.y;
+        }
+		if( (y + height) > (m_height-1) )
+        {
+			return -1;
+        }
+        width_left -= node.width;
+		++i;
+	}
+	return y;
+
+}
+
+void GlyphStash::merge()
+{
+    Node* node;
+    Node* next;
+    uint32_t i;
+
+	for( i=0; i < m_skyline.size()-1; ++i )
+    {
+        node = (Node *) &m_skyline[i];
+        next = (Node *) &m_skyline[i+1];
+		if( node->y == next->y )
+		{
+			node->width += next->width;
+            m_skyline.erase(m_skyline.begin() + i + 1);
+			--i;
+		}
+    }
+}
+
+Rect16 GlyphStash::allocateRegion(uint16_t width, uint16_t height)
+{
+	int y, best_height, best_index;
+    uint32_t best_width;
+    Node* node;
+    Node* prev;
+    Rect16 region(0, 0, width, height);
+    size_t i;
+
+    best_height = INT_MAX;
+    best_index  = -1;
+    best_width = INT_MAX;
+	for( i = 0; i < m_skyline.size(); ++i )
+	{
+        y = fit( i, width, height );
+		if( y >= 0 )
+		{
+            node = &m_skyline[i];
+			if( ( (y + height) < best_height ) ||
+                ( ((y + height) == best_height) && (node->width < best_width)) )
+			{
+				best_height = y + height;
+				best_index = i;
+				best_width = node->width;
+				region.x = node->x;
+				region.y = y;
+			}
+        }
+    }
+
+    if( best_index == -1 )
+    {
+        region.x = -1;
+        region.y = -1;
+        region.w = 0;
+        region.h = 0;
+        return region;
+    }
+    
+    Node newNode(region.x,region.y + height, width);
+    m_skyline.insert(m_skyline.begin() + best_index, newNode);
+
+    for(i = best_index+1; i < m_skyline.size(); ++i)
+    {
+        node = &m_skyline[i];
+        prev = &m_skyline[i-1];
+        if (node->x < (prev->x + prev->width) )
+        {
+            int shrink = prev->x + prev->width - node->x;
+            node->x += shrink;
+            node->width -= shrink;
+            if (node->width <= 0)
+            {
+                 m_skyline.erase(m_skyline.begin() + i);
+                --i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    merge();
+    m_usedSpace += width * height;
+    return region;
+}
+
+// --------------------------------------------------- texture_atlas_upload ---
+/*
+void
+texture_atlas_upload( texture_atlas_t * self )
+{
+    assert( self );
+    assert( self->data );
+
+    if( !self->id )
+    {
+        glGenTextures( 1, &self->id );
+    }
+
+    glBindTexture( GL_TEXTURE_2D, self->id );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    if( self->depth == 4 )
+    {
+#ifdef GL_UNSIGNED_INT_8_8_8_8_REV
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, self->width, self->height,
+                      0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, self->data );
+#else
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, self->width, self->height,
+                      0, GL_RGBA, GL_UNSIGNED_BYTE, self->data );
+#endif
+    }
+    else if( self->depth == 3 )
+    {
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, self->width, self->height,
+                      0, GL_RGB, GL_UNSIGNED_BYTE, self->data );
+    }
+    else
+    {
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_ALPHA, self->width, self->height,
+                      0, GL_ALPHA, GL_UNSIGNED_BYTE, self->data );
+    }
+}
+*/
+}
---
+++ Renderer/GlyphStash.h
@@ -0,0 +1,88 @@
+#pragma once
+
+/// Glyph stash implementation
+/// Inspired from texture-atlas from freetype-gl (http://code.google.com/p/freetype-gl/)
+/// by Nicolas Rougier (Nicolas.Rougier@inria.fr)
+
+/// The actual implementation is based on the article by Jukka Jylänki : "A
+/// Thousand Ways to Pack the Bin - A Practical Approach to Two-Dimensional
+/// Rectangle Bin Packing", February 27, 2010.
+/// More precisely, this is an implementation of the Skyline Bottom-Left
+/// algorithm based on C++ sources provided by Jukka Jylänki at:
+/// http://clb.demon.fi/files/RectangleBinPack/
+
+#include <stdint.h> // uint32_t
+#include <stdlib.h> // size_t
+#include <vector>
+
+namespace bgfx_font
+{
+
+struct Rect16
+{
+    Rect16(uint16_t _x, uint16_t _y, uint16_t _w, uint16_t _h):x(_x), y(_y), w(_w), h(_h) {}
+
+    uint16_t x,y;
+    uint16_t w,h;
+};
+
+//engine abstraction
+class ITextureProvider
+{
+public:
+    virtual void init() = 0;
+    virtual void shutdown() = 0;
+    
+    virtual uint16_t getWidth() = 0;
+    virtual uint16_t getHeight() = 0;
+    virtual uint32_t getDepth() = 0;
+    
+    virtual void update(Rect16 rect, const char* data, uint32_t stride) = 0;
+    virtual void clear() = 0;
+};
+
+struct Node
+{
+    Node(uint16_t _x, uint16_t _y, uint16_t _width):x(_x), y(_y), width(_width) {} 
+    
+    /// The starting x-coordinate (leftmost).
+    uint16_t x;    
+	/// The y-coordinate of the skyline level line.
+	uint16_t y;
+    /// The line width. The ending coordinate (inclusive) will be x+width-1.
+	uint32_t width; //32bit to avoid padding
+};
+
+class GlyphStash
+{    
+public:
+    GlyphStash(ITextureProvider* texture);
+    ~GlyphStash();    
+    Rect16 allocateRegion(uint16_t w, uint16_t h);
+    void updateRegion(Rect16 rect, const char* data, uint32_t stride);
+    float getUsageRatio();
+    void clear();
+private:
+
+    int32_t fit(uint32_t skylineNodeIndex, uint16_t width, uint16_t height);
+	/// Merges all skyline nodes that are at the same level.
+	void merge();
+
+    /// Width (in pixels) of the underlying texture    
+    uint32_t m_width;    
+    /// Height (in pixels) of the underlying texture
+    uint32_t m_height;
+    /// Depth (in bytes) of the underlying texture
+    uint32_t m_depth;
+    
+    /// Surface used in squared pixel
+    uint32_t m_usedSpace;
+
+    /// texture provider (abstract engine)
+    ITextureProvider* m_texture;        
+    
+    /// node of the skyline algorithm
+    std::vector<Node> m_skyline;
+};
+
+}
